f2b8a492a172cfd11ae1b83e2715ca48b23b9f0e
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-04-30 15:40:00 +0530

    work on new virson

--- src/Route/web.php
+++ src/Route/web.php
@@ -1,15 +1,3 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/signin', ['as'=>'login', 'uses'=>'SignmeController@signin']);
 Route::post('/signin-action', ['as'=>'signinAction', 'uses'=>'SignmeController@postSignin']);
